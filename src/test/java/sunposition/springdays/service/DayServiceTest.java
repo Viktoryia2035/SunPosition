e8cbca868f71e7735ba80a99f1e64c5fb44741e6
kopejkinaviktoria3@gmail.com <kopejkinaviktoria3@gmail.com>
2024-04-07 22:48:35 +0300

    fixup! fix

--- src/test/java/sunposition/springdays/service/DayServiceTest.java
+++ src/test/java/sunposition/springdays/service/DayServiceTest.java
@@ -263,52 +263,6 @@ class DayServiceTest {
     }
 
     @Test
-    void testSaveSunriseSunset_RepositoryException() {
-        InMemoryDayDAO repository = mock(InMemoryDayDAO.class);
-        DataCache dayCache = mock(DataCache.class);
-        DayService dayService = new DayService(repository, dayCache);
-
-        Day day = new Day();
-
-        when(repository.save(day)).thenThrow(new RuntimeException("Repository error"));
-
-        try {
-            dayService.saveSunriseSunset(day, "POST");
-            fail("Expected CustomInternalServerErrorException to be thrown");
-        } catch (HttpErrorExceptions.CustomInternalServerErrorException e) {
-            assertTrue(true);
-        } catch (Exception e) {
-            fail("Unexpected exception thrown");
-        }
-
-        verify(dayCache, times(0)).put(anyString(), any(DayDto.class));
-        verify(dayCache, times(0)).clear();
-    }
-
-    @Test
-    void testSaveSunriseSunset_TransactionRollback() {
-        InMemoryDayDAO repository = mock(InMemoryDayDAO.class);
-        DataCache dayCache = mock(DataCache.class);
-        DayService dayService = new DayService(repository, dayCache);
-
-        Day day = new Day();
-
-        when(repository.save(day)).thenThrow(new RuntimeException("Repository error"));
-
-        try {
-            dayService.saveSunriseSunset(day, "POST");
-            fail("Expected CustomInternalServerErrorException to be thrown");
-        } catch (HttpErrorExceptions.CustomInternalServerErrorException e) {
-            assertTrue(true);
-        } catch (Exception e) {
-            fail("Unexpected exception thrown");
-        }
-
-        verify(dayCache, times(0)).put(anyString(), any(DayDto.class));
-        verify(dayCache, times(0)).clear();
-    }
-
-    @Test
     void testFindByLocation_CacheHit() {
         InMemoryDayDAO repository = mock(InMemoryDayDAO.class);
         DataCache dayCache = mock(DataCache.class);
@@ -333,29 +287,6 @@ class DayServiceTest {
         Day day = new Day();
 
         when(repository.save(day)).thenThrow(new IllegalArgumentException("Invalid argument"));
-
-        try {
-            dayService.saveSunriseSunset(day, "POST");
-            fail("Expected CustomInternalServerErrorException to be thrown");
-        } catch (HttpErrorExceptions.CustomInternalServerErrorException e) {
-            assertTrue(true);
-        } catch (Exception e) {
-            fail("Unexpected exception thrown");
-        }
-
-        verify(dayCache, times(0)).put(anyString(), any(DayDto.class));
-        verify(dayCache, times(0)).clear();
-    }
-
-    @Test
-    void testSaveSunriseSunset_TransactionRollback_WithRollback() {
-        InMemoryDayDAO repository = mock(InMemoryDayDAO.class);
-        DataCache dayCache = mock(DataCache.class);
-        DayService dayService = new DayService(repository, dayCache);
-
-        Day day = new Day();
-
-        when(repository.save(day)).thenThrow(new RuntimeException("Repository error"));
 
         try {
             dayService.saveSunriseSunset(day, "POST");
@@ -418,4 +349,34 @@ class DayServiceTest {
                 Arguments.of("Location", "Coordinates", LocalDate.now(), 2, 1)
         );
     }
+
+    @ParameterizedTest
+    @MethodSource("saveSunriseSunsetExceptionScenarios")
+    void testSaveSunriseSunset_Exceptions(Exception repositoryException, Class<? extends Exception> expectedException) {
+        InMemoryDayDAO repository = mock(InMemoryDayDAO.class);
+        DataCache dayCache = mock(DataCache.class);
+        DayService dayService = new DayService(repository, dayCache);
+        Day day = new Day();
+
+        when(repository.save(day)).thenThrow(repositoryException);
+
+        try {
+            dayService.saveSunriseSunset(day, "POST");
+            fail("Expected exception to be thrown");
+        } catch (Exception e) {
+            assertTrue(expectedException.isInstance(e), "Expected exception type: " + expectedException.getName());
+        }
+
+        verify(dayCache, times(0)).put(anyString(), any(DayDto.class));
+        verify(dayCache, times(0)).clear();
+    }
+
+    private static Stream<Arguments> saveSunriseSunsetExceptionScenarios() {
+        return Stream.of(
+                Arguments.of(new RuntimeException("Repository error"), HttpErrorExceptions.CustomInternalServerErrorException.class),
+                Arguments.of(new IllegalArgumentException("Invalid argument"), HttpErrorExceptions.CustomInternalServerErrorException.class),
+                Arguments.of(new RuntimeException("Transaction rollback"), HttpErrorExceptions.CustomInternalServerErrorException.class)
+        );
+    }
+
 }
