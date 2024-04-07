bce1bb08a944b1bb76e65fc534ef989b114dd0da
kopejkinaviktoria3@gmail.com <kopejkinaviktoria3@gmail.com>
2024-04-07 23:32:54 +0300

    fixup! fix

--- src/test/java/sunposition/springdays/SpringDaysApplicationTests.java
+++ src/test/java/sunposition/springdays/SpringDaysApplicationTests.java
@@ -2,15 +2,21 @@ package sunposition.springdays;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-public class SpringDaysApplicationTests {
+class SpringDaysApplicationTests {
+
+    @Autowired
+    private SpringDaysApplication springDaysApplication;
 
     @Test
-    public void contextLoads() {
+    void contextLoads() {
+        assertThat(springDaysApplication).isNotNull();
     }
 
 }
