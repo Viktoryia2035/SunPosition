1627eb91a883922e954d12a54c84908e7649058a
kopejkinaviktoria3@gmail.com <kopejkinaviktoria3@gmail.com>
2024-04-11 02:22:56 +0300

    fixup! fix

--- src/main/java/sunposition/springdays/repository/InMemoryCountryDAO.java
+++ src/main/java/sunposition/springdays/repository/InMemoryCountryDAO.java
@@ -1,13 +1,63 @@
 package sunposition.springdays.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import lombok.AllArgsConstructor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
 import sunposition.springdays.model.Country;
 
+import java.util.List;
 import java.util.Optional;
 
-
+@AllArgsConstructor
 @Repository
-public interface InMemoryCountryDAO extends JpaRepository<Country, Long> {
-    Optional<Country> findByName(String name);
+public class InMemoryCountryDAO {
+
+    private final SessionFactory sessionFactory;
+
+    public Optional<Country> findByName(String name) {
+        return sessionFactory.getCurrentSession().createQuery("SELECT c FROM Country c WHERE c.name = :name", Country.class)
+                .setParameter("name", name)
+                .uniqueResultOptional();
+    }
+
+    public List<Country> findAll() {
+        Session session = sessionFactory.getCurrentSession();
+        Query<Country> query = session.createQuery("SELECT c FROM Country c", Country.class);
+        return query.list();
+    }
+
+    public void save(final Country country) {
+        sessionFactory.inTransaction(session -> session.persist(country));
+    }
+
+    public List<Country> saveAll(List<Country> countries) {
+        Session session = sessionFactory.getCurrentSession();
+        countries.forEach(session::saveOrUpdate);
+        return countries;
+    }
+
+    public Optional<Country> findById(Long id) {
+        Session session = sessionFactory.getCurrentSession();
+        return Optional.ofNullable(session.get(Country.class, id));
+    }
+
+    public void deleteAll() {
+        Session session = sessionFactory.getCurrentSession();
+        Query query = session.createQuery("DELETE FROM Country");
+        query.executeUpdate();
+    }
+
+    public void delete(Country country) {
+        Session session = sessionFactory.getCurrentSession();
+        session.delete(country);
+    }
+
+    public Country saveAndFlush(Country country) {
+        Session session = sessionFactory.getCurrentSession();
+        session.saveOrUpdate(country);
+        session.flush();
+        return country;
+    }
 }
--- src/main/java/sunposition/springdays/repository/InMemoryDayDAO.java
+++ src/main/java/sunposition/springdays/repository/InMemoryDayDAO.java
@@ -1,23 +1,67 @@
 package sunposition.springdays.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import lombok.AllArgsConstructor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.stereotype.Repository;
 import sunposition.springdays.model.Day;
 
 import java.util.List;
 
+@AllArgsConstructor
+@Repository
+public class InMemoryDayDAO {
 
-public interface InMemoryDayDAO extends JpaRepository<Day, Long> {
+    private final SessionFactory sessionFactory;
 
-    Day findByLocation(String location);
+    public Day findByLocation(String location) {
+        Session session = sessionFactory.getCurrentSession();
+        Query<Day> query = session.createQuery("SELECT d FROM Day d WHERE d.location = :location", Day.class);
+        query.setParameter("location", location);
+        return query.uniqueResult();
+    }
 
-    Day findByCoordinates(String coordinates);
+    public List<Day> findAll() {
+        Session session = sessionFactory.getCurrentSession();
+        Query<Day> query = session.createQuery("SELECT d FROM Day d", Day.class);
+        return query.list();
+    }
 
-    @Query("SELECT d FROM Day d JOIN d.country c WHERE c.name = :countryName "
-            + "AND d.weatherConditions = :weatherConditions")
-    List<Day> findByCountryNameAndWeatherConditions(
-            @Param("countryName") String countryName,
-            @Param("weatherConditions") String weatherConditions
-    );
+    public Day save(Day day) {
+        sessionFactory.inTransaction(session -> session.persist(day));
+        return day;
+    }
+
+    public void delete(Day day) {
+        sessionFactory.inTransaction(session -> session.delete(day));
+    }
+
+    public Day findByCoordinates(String coordinates) {
+        Session session = sessionFactory.getCurrentSession();
+        Query<Day> query = session.createQuery("SELECT d FROM Day d WHERE d.coordinates = :coordinates", Day.class);
+        query.setParameter("coordinates", coordinates);
+        return query.uniqueResult();
+    }
+
+    public List<Day> findByCountryNameAndWeatherConditions(String countryName, String weatherConditions) {
+        Session session = sessionFactory.getCurrentSession();
+        Query<Day> query = session.createQuery(
+                "SELECT d FROM Day d JOIN d.country c WHERE c.name = :countryName AND d.weatherConditions = :weatherConditions",
+                Day.class);
+        query.setParameter("countryName", countryName);
+        query.setParameter("weatherConditions", weatherConditions);
+        return query.list();
+    }
+
+    public List<Day> saveAll(List<Day> days) {
+        Session session = sessionFactory.getCurrentSession();
+        days.forEach(session::saveOrUpdate);
+        return days;
+    }
+
+    public void deleteAll(List<Day> days) {
+        Session session = sessionFactory.getCurrentSession();
+        days.forEach(session::delete);
+    }
 }
