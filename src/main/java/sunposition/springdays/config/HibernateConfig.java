fbc7c324cc350d913e97c0b28d8833950d3d4a34
kopejkinaviktoria3@gmail.com <kopejkinaviktoria3@gmail.com>
2024-04-13 21:50:21 +0300

    fixup! fix

--- src/main/java/sunposition/springdays/config/HibernateConfig.java
+++ src/main/java/sunposition/springdays/config/HibernateConfig.java
@@ -1,21 +1,26 @@
 package sunposition.springdays.config;
 
-import sunposition.springdays.model.Day;
-import sunposition.springdays.model.Country;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import sunposition.springdays.model.Country;
+import sunposition.springdays.model.Day;
 
 @Configuration
 public class HibernateConfig {
 
     @Bean
     public SessionFactory sessionFactory() {
-        final StandardServiceRegistry registry =
-                new StandardServiceRegistryBuilder().build();
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure("hibernate.cfg.xml")
+                .applySetting("hibernate.connection.url", "jdbc:postgresql://localhost:5432/postgres?serverTimezone=Europe/Minsk&useSSL=false")
+                .applySetting("hibernate.connection.username", "postgres")
+                .applySetting("hibernate.connection.password", "123")
+                .applySetting("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect")
+                .build();
 
         return new MetadataSources(registry)
                 .addAnnotatedClass(Country.class)
